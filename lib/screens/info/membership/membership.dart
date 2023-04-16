c3ea4b67f7af2134caa0fd81b568e0df4bef3e70
ShivamPundir12 <avi765126@gmail.com>
2023-04-16 14:41:18 +0530

    16-04-23

--- lib/screens/info/membership/membership.dart
+++ lib/screens/info/membership/membership.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:nd_fitness/screens/info/membership/plan_card.dart';
+import 'package:nd_fitness/services/firestore.dart';
 
 import '../../../materials/colors.dart';
 import '../../../services/secure_storage.dart';
@@ -13,6 +14,19 @@ class Membership extends StatefulWidget {
 
 class _MembershipState extends State<Membership> {
   final secure_storage secureStorage = secure_storage();
+
+  String p1t1 = "Bronze Plan";
+  String p1t2 = "1 mo";
+  String p1t3 = "1250";
+  String p2t1 = "Silver Plan";
+  String p2t2 = "3 mo";
+  String p2t3 = "3500";
+  String p3t1 = "Gold Plan";
+  String p3t2 = "6 mo";
+  String p3t3 = "7000";
+  String p4t1 = "Platinum Plan";
+  String p4t2 = "12 mo";
+  String p4t3 = "13000";
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -58,34 +72,67 @@ class _MembershipState extends State<Membership> {
             ),
             Expanded(
               child: SingleChildScrollView(
-                child: InkWell(
-                  onTap: () => Navigator.pushNamedAndRemoveUntil(
-                    context,
-                    "/usrprofile",
-                    ((route) => false),
-                  ),
-                  child: Container(
-                    child: Column(
-                      children: [
-                        PlanCard(
-                          t1: "Bronze Plan",
-                          t2: "1 mo",
-                          t3: "1250",
+                child: Container(
+                  child: Column(
+                    children: [
+                      GestureDetector(
+                        child: PlanCard(
+                          t1: p1t1,
+                          t2: p1t2,
+                          t3: p1t3,
                         ),
-                        SizedBox(
-                          height: 30,
+                        onTap: () async {
+                          await Firecloud.addplandetail(p1t1, p1t2, p1t3);
+                          Navigator.pushNamedAndRemoveUntil(
+                              context, "/allset", (route) => false);
+                        },
+                      ),
+                      SizedBox(
+                        height: 30,
+                      ),
+                      GestureDetector(
+                        child: PlanCard(
+                          t1: p2t1,
+                          t2: p2t2,
+                          t3: p2t3,
                         ),
-                        PlanCard(t1: "Silver Plan", t2: "3 mo", t3: "3500"),
-                        SizedBox(
-                          height: 30,
+                        onTap: () async {
+                          await Firecloud.addplandetail(p2t1, p2t2, p2t3);
+                          Navigator.pushNamedAndRemoveUntil(
+                              context, "/allset", (route) => false);
+                        },
+                      ),
+                      SizedBox(
+                        height: 30,
+                      ),
+                      GestureDetector(
+                        child: PlanCard(
+                          t1: p3t1,
+                          t2: p3t2,
+                          t3: p3t3,
                         ),
-                        PlanCard(t1: "Gold Plan", t2: "6 mo", t3: "7000"),
-                        SizedBox(
-                          height: 30,
+                        onTap: () async {
+                          await Firecloud.addplandetail(p3t1, p3t2, p3t3);
+                          Navigator.pushNamedAndRemoveUntil(
+                              context, "/allset", (route) => false);
+                        },
+                      ),
+                      SizedBox(
+                        height: 30,
+                      ),
+                      GestureDetector(
+                        child: PlanCard(
+                          t1: p4t1,
+                          t2: p4t2,
+                          t3: p4t3,
                         ),
-                        PlanCard(t1: "Platinum Plan", t2: "12 mo", t3: "13000")
-                      ],
-                    ),
+                        onTap: () async {
+                          await Firecloud.addplandetail(p4t1, p4t2, p4t3);
+                          Navigator.pushNamedAndRemoveUntil(
+                              context, "/allset", (route) => false);
+                        },
+                      ),
+                    ],
                   ),
                 ),
               ),
